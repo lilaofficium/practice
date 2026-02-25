e66bc0a37a284c99481660cca2c0faecbe4517f9
lila Yadav <lila.yadavofficium@gmail.com>
2026-02-25 16:45:19 +0545

    data type and variable basic.

---
+++ Basic/Variables_DataType.py
@@ -0,0 +1,41 @@
+age = 25 
+salary = 75000.50 
+name = "Rahul" 
+is_active = True 
+data = None 
+#print(type(age))# output =<class 'int'>  
+#print(type(age).__name__)# output =int
+
+#Type Casting
+x = "100"
+y = int(x)
+#print(type(y).__name__)# output =int
+a = 10
+b = float(a)
+#print(type(b).__name__)# output =float
+
+#Operators
+a = 10
+b = 3 
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a % b)
+# print(a // b)
+# print(a ** b)
+
+#Comparison
+# print(a > b)
+# print(a == b)
+# print(a != b)
+
+#Logical
+
+x = True
+y = False
+
+print(x and y)
+print(x or y)
+print(not x)
